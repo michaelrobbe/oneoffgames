ec57188e9290c6d4b718b4376e78d8d7a966fbc7
michaelrobbe <mrrobbe@gmail.com>
2014-08-08 01:16:30 -0700

    Updated tictactoe to track mouse movements, added player turns, and alter cells

--- tictactoe.php
+++ tictactoe.php
@@ -3,35 +3,85 @@
 <head>
   <meta charset="UTF-8">
   <title>Tic·Tac·Toe</title>
-    <script src="js/vendor/jquery.js"></script>
+    <style>
+      body {
+        font-family: "Arial";
+        text-align: center;
+        color: #000;
+      }
+
+      .table {
+        display: table;
+        line-height: 50px;
+      }
+
+      .table_row {
+        display: table-row;
+        float: left;
+        clear: left;
+        border: solid #ededed;
+        border-width: 1px 0;
+      }
+
+      .table_cell {
+        display: table-cell;
+        width: 50px;
+        height: 50px;
+        /*border: 1px solid black;*/
+      }
+
+      .table_cell:nth-child(2n) {
+        background-color: #ccc;
+      }
+
+      a {
+        border: none;
+        width: 50px;
+        height: 50px;
+        font-size: 30px;
+        background-color: #aac;
+        color: #fff;
+        display: inline-block;
+        text-decoration: none;
+      }
+
+      a.active {
+        border: 1px solid #a0f;
+      }
+
+      .tictac_picker {
+        padding-bottom: 20px;
+      }
+    </style>
     <script type="text/javascript">
+
+var i = 1;
+
+var gridSize = 3;
 
 var buildGrid = function(x, y, gridSize) {
   var output = new String();
-  var count = (x+1) + (y * x);
 
   if (x === 0 && y === 0) {
-    output += '<table>';
+    output += '<div class="table">';
   };
 
   if (x === 0) {
-    output += '<tr>';
+    output += '<div class="table_row">';
   };
 
-  output += '<td>' + count + '</td>';
+  output += '<div class="table_cell">' + i + '</div>';
 
-  if (x === gridSize) {
-    output += '</tr>';
+  if (x === gridSize-1) {
+    output += '</div>';
   };
 
   if (x === gridSize && y === gridSize) {
-    output += '</table>';
+    output += '</div>';
   };
-
+  i++;
   return output;
 };
-
-var gridSize = 3;
 
 var renderGrid = function() {
   for (y = 0; y < gridSize; y++) {
@@ -41,11 +91,67 @@ var renderGrid = function() {
   };
 }
 
+function clickHandler(e) {
+  var elem, evt = e ? e:event;
+
+  if (evt.srcElement) {
+    elem = evt.srcElement;
+  }
+  else if (evt.target) {
+    elem = evt.target;
+  }
+
+  if (Number(elem.innerHTML).isInteger()) {
+    console.log(elem.innerHTML);
+    elem.innerHTML = activePlayer();
+  }
+
+  // alert (elem.innerHTML)
+  changePlayer();
+  return true;
+}
+
+document.onclick = clickHandler;
+
+// window.onclick = markGrid();
+
+function changePlayer() {
+  var playerEx = document.getElementById("ex");
+  var playerOh = document.getElementById("oh");
+
+  if (playerEx.className === 'active') {
+    playerEx.removeAttribute('class');
+    playerOh.setAttribute('class', 'active');
+  }
+  else {
+    playerOh.removeAttribute('class');
+    playerEx.setAttribute('class', 'active');
+  }
+}
+
+function activePlayer() {
+  var playerEx = document.getElementById("ex");
+  var playerOh = document.getElementById("oh");
+
+  if (playerEx.className === 'active') {
+    return 'X';
+  }
+  else {
+    return 'O';
+  }
+}
+
     </script>
 </head>
 <body>
+  <div class="tictac_picker">
+    <a href="javascript:void(0);" id="ex" class="active" onClick="changePlayer();">X</a>
+    <a href="javascript:void(0);" id="oh" onClick="changePlayer();" >O</a>
+  </div>
   <div class="tictac_wrapper">
-    <script type="text/javascript">renderGrid()</script>
+    <script type="text/javascript">
+      renderGrid();
+    </script>
   </div>
 </body>
 </html>
